9064380ae528afbf8d3d442f82a383cb4bbaeff7
BuildTools <claire.cate@yahoo.com>
2026-05-04 17:04:43 -0600

    add gating for combination method types

--- recommender.py
+++ recommender.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from collections import Counter
 from Recomender_Helper.vector_helper import get_vector_by_isbn, cosine_similarity, average_vectors, concat, concat_with_weight
 from Vectorizer.EmotionConditionedTopicVectorMaker import EmotionConditionedTopicVectorMaker
+from Vectorizer.CorrelationCombo import combine_vectors
 
 # ---- Paths ----
 
@@ -14,13 +15,16 @@ STEM_BOOKS_FILE = "processed_data/stem_isbns_from_topic.txt"
 emotion_conditioned_vector_maker = EmotionConditionedTopicVectorMaker()
 
 
-def handle_book(isbn, emotion_type, topic_type, emotion_weight = 1.0, topic_weight = 1.0):
+def handle_book(isbn, emotion_type, topic_type, emotion_weight = 1.0, topic_weight = 1.0, use_matrix_combo = False, reduce = False):
     emotion_vec = get_vector_by_isbn(isbn, emotion_type)
     if emotion_vec is None:
         raise Exception(f"Do not have vector for {isbn}")
     topic_vec = get_vector_by_isbn(isbn, topic_type)
 
-    return concat_with_weight(emotion_vec, topic_vec, emotion_weight, topic_weight)
+    if use_matrix_combo:
+        return combine_vectors(emotion_vec, emotion_type, topic_vec, topic_type, reduce, emotion_weight)
+    else:
+        return concat_with_weight(emotion_vec, topic_vec, emotion_weight, topic_weight)
 
 def general_stem_topic_vec_maker(topic_type):
     stem_isbns = set()
@@ -41,7 +45,7 @@ def general_stem_topic_vec_maker(topic_type):
     #print(f"no vector for {missing_stem_isbn} STEM books")
     return average_vectors(stem_vecs)
 
-def make_candidate_profile(profile_books, emotion_type, general_stem_topic_vec, emotion_weight = 1.0, topic_weight = 1.0):
+def make_candidate_profile(profile_books, emotion_type, general_stem_topic_vec, topic_type, emotion_weight = 1.0, topic_weight = 1.0, use_matrix_combo = False, reduce = False):
     emotion_vectors = []
     for book in profile_books:
         isbn = book['isbn']
@@ -54,11 +58,14 @@ def make_candidate_profile(profile_books, emotion_type, general_stem_topic_vec, 
     
     emotion_profile = average_vectors(emotion_vectors)
 
-    return concat_with_weight(emotion_profile, general_stem_topic_vec, emotion_weight, topic_weight)
+    if use_matrix_combo:
+        return combine_vectors(emotion_profile, emotion_type, general_stem_topic_vec, topic_type, reduce, emotion_weight)
+    else:
+        return concat_with_weight(emotion_profile, general_stem_topic_vec, emotion_weight, topic_weight)
 
 
 
-def recomend(test_data_file, emotion_type = "emotion_intensity", topic_type = "tf_idf", emotion_weight = 1.0, topic_weight = 1.0):
+def recomend(test_data_file, emotion_type = "emotion_intensity", topic_type = "tf_idf", emotion_weight = 1.0, topic_weight = 1.0, use_matrix_combo = False, reduce = False):
     general_stem_topic_vec = general_stem_topic_vec_maker(topic_type)
     with open(test_data_file, 'r') as file:
         data = json.load(file)
@@ -66,11 +73,11 @@ def recomend(test_data_file, emotion_type = "emotion_intensity", topic_type = "t
     for item in tqdm(data):
         profile_books = item['candidate_profile']
         try:
-            candidate_profile = make_candidate_profile(profile_books, emotion_type, general_stem_topic_vec, emotion_weight, topic_weight)
+            candidate_profile = make_candidate_profile(profile_books, emotion_type, general_stem_topic_vec, topic_type, emotion_weight, topic_weight, use_matrix_combo, reduce)
             recomendation_books = item['recommendation_list']
             for book in recomendation_books:
                 try:
-                    book_vec = handle_book(book['isbn'], emotion_type, topic_type, emotion_weight, topic_weight)
+                    book_vec = handle_book(book['isbn'], emotion_type, topic_type, emotion_weight, topic_weight, use_matrix_combo, reduce)
                     #book_vec = get_vector_by_isbn(book['isbn'], emotion_type)
                     cos = cosine_similarity(candidate_profile, book_vec)
                     book['cos'] = cos
