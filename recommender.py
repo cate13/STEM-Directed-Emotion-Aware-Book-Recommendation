968c7d8292147e8c22ea3c2d5d3710f4abe292b1
BuildTools <claire.cate@yahoo.com>
2026-05-06 10:34:23 -0600

    file rename

--- recommender.py
+++ recommender.py
@@ -10,7 +10,7 @@ import numpy as np
 
 # ---- Paths ----
 
-TEST_DATA_FILE = "user_eval_sets/users_1_plus_STEM_books_and_6_plus_high_rated_split_60_40.json"
+TEST_DATA_FILE = "user_eval_sets/users_1_plus_STEM_books_and_10_plus_high_rated_split_60_40.json"
 #TEST_DATA_FILE = "user_eval_sets/test.json"
 STEM_BOOKS_FILE = "processed_data/stem_isbns_from_topic.txt"
 
